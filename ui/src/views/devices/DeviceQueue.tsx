a74620066612ce739615e576b6c52dd62ff599a9
Johannes Koning <j.koning@teneo-iot.nl>
2026-09-10 08:32:06 +0200

    ui: change fport default value to null to not accidentally forget changing it and to prevent mistakenly sending commands on port 1

--- ui/src/views/devices/DeviceQueue.tsx
+++ ui/src/views/devices/DeviceQueue.tsx
@@ -255,7 +255,7 @@ function DeviceQueue(props: IProps) {
                 <Switch />
               </Form.Item>
               <Form.Item name="fPort" label="FPort">
-                <InputNumber min={1} max={254} />
+                <InputNumber min={1} max={254} defaultValue={null} />
               </Form.Item>
               <Form.Item
                 name="isEncrypted"
